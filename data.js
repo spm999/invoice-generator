13492a99cb6c814116492af0a73b272bf09b7051
Developers Today <mshari7185@gmail.com>
2024-06-05 15:33:19 +0530

    dummy data for invoice

---
+++ data.js
@@ -0,0 +1,53 @@
+const data = {
+            seller: {
+                name: 'Varasiddhi Silk Exports',
+                address: '75, 3rd Cross, Lalbagh Road',
+                city: 'BENGALURU',
+                state: 'KARNATAKA',
+                pincode: '560027',
+                panNo: 'AACFV3325K',
+                gstNo: '29AACFV3325K1ZY'
+            },
+            placeOfSupply: 'KARNATAKA',
+            billing: {
+                name: 'Madhu B',
+                address: 'Eurofins IT Solutions India Pvt Ltd., 1st Floor, Maruti Platinum, Lakshminarayana Pura, AECS Layou',
+                city: 'BENGALURU',
+                state: 'KARNATAKA',
+                pincode: '560037',
+                stateCode: '29'
+            },
+            shipping: {
+                name: 'Madhu B',
+                address: 'Eurofins IT Solutions India Pvt Ltd., 1st Floor, Maruti Platinum, Lakshminarayana Pura, AECS Layou',
+                city: 'BENGALURU',
+                state: 'KARNATAKA',
+                pincode: '560037',
+                stateCode: '29'
+            },
+            placeOfDelivery: 'KARNATAKA',
+            orderNo: '403-3225714-7676307',
+            orderDate: '28.10.2019',
+            invoiceNo: 'IN-761',
+            invoiceDate: '28.10.2019',
+            reverseCharge: 'No',
+            items: [
+                {
+                    description: 'Varasiddhi Silks Men\'s Formal Shirt (SH-05-42, Navy Blue, 42)',
+                    unitPrice: 338.10,
+                    quantity: 1,
+                    discount: 0,
+                    taxRate: 18 // in percentage
+                },
+                
+                {
+                    description: 'Varasiddhi Silks Men\'s Formal Shirt (SH-05-40, Navy Blue, 40)',
+                    unitPrice: 338.10,
+                    quantity: 1,
+                    discount: 0,
+                    taxRate: 18 // in percentage
+                }
+            ]
+        };
+
+        module.exports = { data };
